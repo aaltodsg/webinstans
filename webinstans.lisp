b319a4f7e06aa60f379223c539fc31ef0c4ea653
Esko Nuutila <Esko.Nuutila@aalto.fi>
2015-10-26 13:55:43 +0200

    .

--- webinstans.lisp
+++ webinstans.lisp
@@ -56,6 +56,61 @@
         do (setf object `((@ ,object ,call) ,@params))
         finally (return object)))
 
+(defclass instans-connection (hunchensocket:websocket-resource)
+  ((name :initarg :name :initform (error "Name this connection!") :reader name))
+  (:default-initargs :client-class 'user))
+
+(defclass instans-user (hunchensocket:websocket-client)
+  ((name :initarg :name :reader name :initform (error "Name this user!"))))
+
+(defun broadcast (connection message &rest args)
+  (loop for peer in (hunchensocket:clients connection)
+        do (hunchensocket:send-text-message peer (apply #'format nil message args))))
+
+(defmethod hunchensocket:client-connected ((connection instans-connection) user)
+  (broadcast connection "~a has joined ~a" (name user) (name connection)))
+
+(defmethod hunchensocket:client-disconnected ((connection instans-connection) user)
+  (broadcast connection "~a has left ~a" (name user) (name connection)))
+
+(defmethod hunchensocket:text-message-received ((connection instans-connection) user message)
+  (broadcast connection "~a says ~a" (name user) message))
+
+(defvar *instans-web-servers* nil)
+
+(defclass instans-web-server ()
+  ((acceptor :accessor instans-web-server-acceptor :initarg :acceptor)
+   (instans :accessor instans-web-server-instans :initarg :instans :initform nil)
+   (connection :accessor instans-web-server-connection :initarg :connection :initform nil)))
+
+(defvar *instans-connections* nil)
+
+;; (defun find-instans (request)
+;;   (find (hunchentoot:script-name request) *instans-connections* :test #'string= :key #'name))
+
+(defun logmsg (msg)
+  (with-open-file (str "log" :direction :output :if-exists :append :if-does-not-exist :create)
+    (format str "~%~A~%" msg)))
+
+(defun try-create-and-start-instans-web-server (port)
+  (let ((acceptor (make-instance 'hunchensocket:websocket-acceptor :port port)))
+    (logmsg (format nil "try-create-and-start-instans-web-server ~D" port))
+    (hunchentoot:start acceptor)
+    (let ((server (make-instance 'instans-web-server :acceptor acceptor :connection nil ; (make-instance 'instans-connection :name (format nil "/instans~D" port))
+				 )))
+      (push server *instans-web-servers*)
+;      (pushnew 'find-instans hunchensocket:*websocket-dispatch-table*)
+      server)))
+
+(defun create-and-start-instans-web-server ()
+  (logmsg "create-and-start-instans-web-server")
+  (loop for port from 2048
+	do (handler-case
+	       (let ((server (try-create-and-start-instans-web-server port)))
+		 (logmsg (format nil "create-and-start-instans-web-server ~A" server))
+		 (return-from create-and-start-instans-web-server server))
+	     (t ()))))
+
 (define-easy-handler (home-page :uri "/configure") ()
   (setf (content-type*) "text/html")
   (let* ((split-command-cases (loop for case in instans::*instans-command-cases*
@@ -106,6 +161,9 @@
 ;; 							    (if (= type "file")
 ;; 								(alert ((@ ($ this) text))))))))
 ;; 				))))))
+	 (instans-web-server (create-and-start-instans-web-server))
+	 (port (hunchentoot::acceptor-port (instans-web-server-acceptor instans-web-server)))
+	 ;; (port 24567)
 	 (head (cl-who::with-html-output-to-string (*standard-output* nil :prologue nil :indent t)
 		 (:head 
 		  (:meta :charset "utf-8")
@@ -115,23 +173,22 @@
 		  (:script :src "http://code.jquery.com/jquery-1.10.2.js")
 		  (:script :src "http://code.jquery.com/ui/1.10.4/jquery-ui.js")
 		  (:script :src "http://www.cs.hut.fi/~enu/service.flextabledit.jquery.min.js")
+		  (:script (cl-who:str (eval `(ps (ps:var ws-uri ,(format nil "ws://localhost:~D//instans~D" port port))))))
+		  (:script :src "http://www.cs.hut.fi/~enu/socket.js")
 		  (:style (cl-who:str (configurator-css)))
 ;		  (:script (cl-who::str add-parameter))
 		  (:script (cl-who:str
-			    (ps ($ (lambda ()
-				     ((@ ($ "#tabs") tabs)
-				      (create :active 0))
-;				     (alert ($ "#parameters"))
-;				     (add-parameter)
-;				     (alert ($ "#paramform"))
-				     (callchain ($ "#paramform") submit ((lambda (event)
-									   (let ((params (callchain ($ "#parameters") val)))
-									     (alert (concatenate 'string "About to launch INSTANS with parameters " params))))))
-				     )))))
+			    (eval `(ps ($ (lambda ()
+					    ((@ ($ "#tabs") tabs)
+					     (create :active 0))
+					    (callchain ($ "#paramform") submit ((lambda (event)
+										  (let ((params (callchain ($ "#parameters") val)))
+										    (alert (concatenate 'string "About to launch INSTANS with parameters " params " on port " ,(format nil "~D" port)))))))
+					    ))))))
 		  )))
 	 (body (cl-who::with-html-output-to-string (*standard-output* nil :indent t)
 		 (:body
-		  (cl-who::str select-options-html)
+;		  (cl-who::str select-options-html)
 		  (:div :id "tabs"
 			(:ul
 			 (:li (:a :href "#configure-tab" "Configure"))
@@ -143,7 +200,8 @@
 				     (:input :type "submit" :value "Launch")
 				     ;; (:fieldset
 				     ;;  (:ol :id "parameters"))
-				     ))
+				     )
+			      (:div :id "messages"))
 			(:div :id "execute-tab" (:p "Execute"))))))
 	 (output (cl-who::with-html-output-to-string (*standard-output* nil :prologue t :indent t)
 		   (:html :xmlns "http://www.w3.org/1999/xhtml"
