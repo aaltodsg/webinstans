0d4de6fbd198830ad25d105ee3ab36b81b1341b9
Esko Nuutila <Esko.Nuutila@aalto.fi>
2015-11-24 15:04:51 +0200

    Added state to tracing, not tested yet

--- webinstans-macros.lisp
+++ webinstans-macros.lisp
@@ -2,18 +2,22 @@
 
 (in-package :webinstans)
 
-(defmacro trace-wrap-call (call)
-  (let ((call-var (gensym "CALL")))
-    `(let ((,call-var ,call))
+(defmacro trace-wrap-call (call &key state-form)
+  (let ((call-var (gensym "CALL"))
+	(state-var (gensym "STATE")))
+    `(let ((,call-var ,call)
+	   ,@(if state-form `((,state-var ,state-form))))
        (when *instans-trace*
-	 (logmsg "enter ~A" ,call-var)
-	 (instans-trace-add-enter *instans-trace* ,call-var))
+	 ,(if state-form `(logmsg "enter ~A, state ~A" ,call-var ,state-var) `(logmsg "enter ~A" ,call-var))
+	 (instans-trace-add-enter *instans-trace* :call ,call-var ,@(if state-form `(:state ,state-form))))
        (multiple-value-prog1 (call-next-method)
+	 ,@(if state-form `((setf ,state-var ,state-form)))
 	 (when *instans-trace*
-	   (logmsg "exit ~A" ,call-var)
-	   (instans-trace-add-exit *instans-trace* ,call-var))))))
+	   ,(if state-form `(logmsg "exit ~A, state ~A" ,call-var ,state-var) `(logmsg "exit ~A" ,call-var))
+	   (instans-trace-add-exit *instans-trace* :call ,call-var ,@(if state-form `(:state ,state-form))))))))
 
 (defmacro loggingmsgs (&body body)
   `(with-open-file (str "log" :direction :output :if-exists :append :if-does-not-exist :create)
      (let ((instans::*stream-open-close-report-output* str))
        ,@body)))
+
--- webinstans.lisp
+++ webinstans.lisp
@@ -2,23 +2,31 @@
 
 (in-package :webinstans)
 
-(defun main (args)
+(defun tracing-main (args)
+  (cond ((null args)
+	 (setf args (list "")))
+	((stringp args)
+	 (setf args (list args))))
   (multiple-value-bind (trace-options other)
       (instans::extract-options args '("trace"))
-    (instans::inform "Got tracing arguments ~S" trace-options)
-    (instans::inform "Other args ~S" other)
+    ;; (instans::inform "Got tracing arguments ~S" trace-options)
+    ;; (instans::inform "Other args ~S" other)
+    (logclean)
     (when trace-options
       (when (not (= 1 (length trace-options)))
 	(error "Expecting just one tracing argument in ~A" args))
       (instans-trace-init))
-    (multiple-value-prog1 (instans::main (list (instans::parsed-options-to-string other)))
-      (when trace-options
-	(let ((trace-output-file (second (first trace-options))))
-	  (cond ((null trace-output-file)
-		 (instans-trace-print *instans-trace*))
-		(t
-		 (with-open-file (output trace-output-file :if-exists :supersede)
-		   (instans-trace-print *instans-trace* output)))))))))
+    (let ((main-options (instans::parsed-options-to-string other)))
+      ;; (instans::inform "Haa ~S" main-options)
+      (multiple-value-prog1
+	  (instans::main main-options)
+	(when trace-options
+	  (let ((trace-output-file (second (first trace-options))))
+	    (cond ((null trace-output-file)
+		   (instans-trace-print *instans-trace*))
+		  (t
+		   (with-open-file (output trace-output-file :direction :output :if-exists :supersede)
+		     (instans-trace-print *instans-trace* output))))))))))
 
 
 	    
